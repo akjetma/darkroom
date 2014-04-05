e5951bcad9c15396d89372bcc851aaa4d0c18a7e
Adam Jetmalani <adam@getlittlebird.com>
2014-04-05 03:10:10 -0700

    so sleepy

--- lib/darkroom/endpoints/album.rb
+++ lib/darkroom/endpoints/album.rb
@@ -2,9 +2,9 @@ module Darkroom
   class Album
     @resources = {
 
-      :info =>   lambda { |args| ['album', args[0]] },
+      :info   => lambda { |args| ['album', args[0]] },
       :images => lambda { |args| ['album', args[0], 'images'] }
-      
+
     }
   end
 end
