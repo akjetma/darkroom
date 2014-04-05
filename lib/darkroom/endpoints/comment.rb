534fadaaa32349e12e1cc970f4128d733840f396
Adam Jetmalani <adam@getlittlebird.com>
2014-04-05 03:21:32 -0700

    time to sleep

--- lib/darkroom/endpoints/comment.rb
+++ lib/darkroom/endpoints/comment.rb
@@ -2,9 +2,9 @@ module Darkroom
   class Comment
     @resources = {
 
-      :info    => lambda { |args| ['comment', args[0]] }
+      :info    => lambda { |args| ['comment', args[0]] },
       :replies => lambda { |args| ['comment', args[0], 'replies'] }
-      
+
     }
   end
 end
