750416218db56ba37b62ca9413409506591944ed
Adam Jetmalani <adam@getlittlebird.com>
2014-04-05 18:12:36 -0700

    dynamic attr accessors for response properties. could explicitly map out schema for speed/clarity, but this has nice properties too

--- lib/darkroom/utility/config.rb
+++ lib/darkroom/utility/config.rb
@@ -6,12 +6,45 @@ module Darkroom
 
   def self.build
     [Image, Album, Comment].each do |klass|
-      klass.instance_eval do
-        @resources.keys.each do |name|
-          define_singleton_method(name) do |*args|
-            path = @resources[name].call(args).join('/')
-            Root.get(path)
-          end
+      def_initialize(klass)
+      def_endpoints(klass)
+      set_dynamic_accessors(klass)
+    end
+  end
+
+  def self.def_initialize(klass)
+    klass.class_eval do
+      def initialize(response)
+        @properties = []
+        response.each_pair do |key, val|
+          key = "@#{key}".to_sym
+          @properties << key
+          instance_variable_set(key, val)
+        end
+      end
+    end
+  end
+
+  def self.def_endpoints(klass)
+    klass.instance_eval do
+      @resources.keys.each do |name|
+        define_singleton_method(name) do |*args|
+          path = @resources[name].call(args).join('/')
+          response = Root.get(path)
+          self.new(response['data'])
+        end
+      end
+    end
+  end
+
+  def self.set_dynamic_accessors(klass)
+    klass.class_eval do
+      define_method(:method_missing) do |name, *args|
+        prop = "@#{name}".to_sym
+        if @properties.include?(prop)
+          instance_variable_get(prop)
+        else
+          super
         end
       end
     end
